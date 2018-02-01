1406301a636c007c0e4cf0b1b10b0626e655f5ab
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-02-01 13:37:52 -0600

    exercise finished yeaaa

--- converter.js
+++ converter.js
@@ -1,35 +1,61 @@
 
 
-var input = document.getElementById("tempInput").value;
-
-
 var temp = "";
 
 
 // check for radio button
 function check(input) {
+
+    var input = document.getElementById("tempInput").value;
     
     if (document.getElementById("fehrenheit").checked === true) {
-        
         // convert fehrenheit to celsius
-        // Deduct 32, then multiply by 5, then divide by 9
-        
-        var temp = (((parseInt(input) - 32) * 5) / 9);
-        console.log(temp);
-        return temp;
-        
-    } else {
-        
-        // convert celsius to fehrenheit
-        // Multiply by 9, then divide by 5, then add 32
-        
-        var temp = (((parseInt(input) * 9) / 5) + 32);
-        console.log(temp);
-        return temp;
-        
+        var x = (((parseInt(input) - 32) * 5) / 9);
+            if (x > 32) {
+                var temp = `<h3 style = "color:red;">${x} degrees Celsius</h3>`;
+            } else if (x < 0) {
+            var temp = `<h3 style = "color:blue;">${x} degrees Celsius</h3>`;
+            } else if (x <= 32 && x >= 0) { 
+                var temp = `<h3 style = "color:green;">${x} degrees Celsius</h3>`;
+            } else {
+                var temp = `<h3 style = "color:black;">Please enter a valid tempetature.</h3>`;
+            }
+ 
+    } else if (document.getElementById("celsius").checked === true && document.getElementById("fehrenheit").checked === false) {
+        // convert celsius to fehrenheit 
+        var x = (((parseInt(input) * 9) / 5) + 32);
+        if (x > 90) {
+            var temp = `<h3 style = "color:red;">${x} degrees Fehrenheit</h3>`;
+        } else if (x < 32) { 
+            var temp = `<h3 style = "color:blue;">${x} degrees Fehrenheit</h3>`;
+        } else if (x <= 90 && x >= 32) {
+            var temp = `<h3 style = "color:green;">${x} degrees Fehrenheit</h3>`;
+        } else {
+            var temp = `<h3 style = "color:black;">Please enter a valid tempetature.</h3>`;
+        }
+    }
+    
+    document.getElementById("tempOutput").innerHTML = temp;
+}
+
+function clear() {
+    document.getElementById("tempInput").value = "";
+}
+
+function enter(event) {
+    if (event.keyCode === 13) {
+    console.log("working");
+        check();
     }
 }
 
+// on click
 document.getElementById("convert").addEventListener("click", check);
-
-document.getElementById("tempOutput").innerHTML = temp;
+// clear the input
+document.getElementById("clear").addEventListener("click", clear);
+// on enter input
+document.getElementById("tempInput").addEventListener("keypress", enter);
+// on  enter fehrenheit
+document.getElementById("fehrenheit").addEventListener("keypress", enter);
+// on enter celsius
+document.getElementById("celsius").addEventListener("keypress", enter);
